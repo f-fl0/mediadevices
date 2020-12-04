2f83176ea18ecf50e49cf43183d0321ba43b174c
Lukas Herman <lherman.cs@gmail.com>
2020-12-04 12:11:29 -0800

    Working build script for openh264

--- pkg/codec/openh264/build.sh
+++ pkg/codec/openh264/build.sh
@@ -4,8 +4,16 @@ SRC_DIR=src
 LIB_DIR=lib
 INCLUDE_DIR=include
 ROOT_DIR=${PWD}
+LIB_PREFIX=libopenh264
+
+# TODO: map target platform to OS and ARCH
+OS=linux
+ARCH=arm
+
+mkdir -p ${LIB_DIR} ${INCLUDE_DIR}
 
 git clone --depth=1 --branch=${VERSION} ${GIT_URL} ${SRC_DIR}
 cd ${SRC_DIR}
-${MEDIADEVICES_TOOLCHAIN_BIN} make -j
+${MEDIADEVICES_TOOLCHAIN_BIN} make -j OS=${OS} ARCH=${ARCH}
+mv ${LIB_PREFIX}.a ${ROOT_DIR}/${LIB_DIR}/${LIB_PREFIX}_${MEDIADEVICES_TARGET_PLATFORM}.a
 git clean -dfx

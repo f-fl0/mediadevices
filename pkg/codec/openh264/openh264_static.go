037602634fa2f8100c0dc321789876b31e4781f4
Lukas Herman <lherman.cs@gmail.com>
2020-12-07 11:09:53 -0800

    Update

--- pkg/codec/openh264/openh264_static.go
+++ pkg/codec/openh264/openh264_static.go
@@ -7,5 +7,5 @@ package openh264
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264_linux_arm64.a
 //#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264_linux_x64.a
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264_darwin_x64.a
-//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264_windows_x64.a
+//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264_windows_x64.a -lssp
 import "C"
